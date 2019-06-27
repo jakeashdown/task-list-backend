d01a53dd74d9446b644330a68fcc92a660a7ce46
jake.ashdown@protonmail.com <jake.ashdown@protonmail.com>
2019-06-27 17:34:53 +0200

    Initial commit

---
+++ src/main/java/com/jakeashdown/restbackenddemo/RestBackendDemoApplication.java
@@ -0,0 +1,13 @@
+package com.jakeashdown.restbackenddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestBackendDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestBackendDemoApplication.class, args);
+	}
+
+}
